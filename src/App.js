277d0cd053b7f43f3aed2b9f6aa3c86a3c343ec5
Gustavo Specht <guspecht@hotmail.com>
2022-10-25 20:56:46 +0800

    States and shallow merge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Gustavo'
+      name: {firstName: 'Gustavo', lastName: 'Specht'},
+      company: 'Specht Company'
     }
   }
 
@@ -17,11 +18,13 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name}
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
           </p>
           <button onClick={() => {
           this.setState({
-            name: 'Gus'})
+            name: { firstName : 'Anderson', lastName: 'Specht'}
+          })
+          console.log(this.state)
             }}
           > 
           Change Name
