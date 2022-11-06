f3faddd06aa2b90a7a0354cd98874afd25ccc60c
Gustavo Specht <guspecht@hotmail.com>
2022-11-06 13:32:32 +0800

    Search Box Component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,7 +2,7 @@ import { Component } from 'react';
 
 class CardList extends Component {
     render() {
-        console.log('render');
+        console.log('render from card component');
         const { monsters } = this.props;
         return (
             <div>
